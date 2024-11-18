05ff41d2ed1c2e91cf8e79b2ebe0347582ff7dc4
Hinano <yuyefeinainai@gmail.com>
2024-11-18 23:31:03 +0900

    Initial Commit

---
+++ Alphabetizer/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by 宇野緋奈乃 on 2024/11/17.
+//
+
+import Foundation
+
+
+enum Message: String {
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}
---
+++ Alphabetizer/Views/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Alphabetizer
+//
+//
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            ScoreView()
+            MessageView()
+            Spacer()
+            WordCanvas()
+            Spacer()
+            SubmitButton()
+        }
+        .padding(.top, 50)
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(Alphabetizer())
+}
